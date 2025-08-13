5bd87f59ad0ad15851f05de13e51fd159783f97b
Roman <romanorlandoboc@gmail.com>
2025-08-13 13:56:51 -0500

    v0.1_clasesPqueteModelo_Roman_13082025

--- src/main/java/com/sena/ecommerce/SpringEcommerce3063267Application.java
+++ src/main/java/com/sena/ecommerce/SpringEcommerce3063267Application.java
@@ -2,6 +2,7 @@ package com.sena.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class SpringEcommerce3063267Application {
---
+++ src/main/java/com/sena/ecommerce/model/Orden.java
@@ -0,0 +1,5 @@
+package com.sena.ecommerce.model;
+
+public class Orden {
+
+}
